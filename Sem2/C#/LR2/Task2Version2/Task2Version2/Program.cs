930e5f2db840c2ee9bd1f11254ebcba2efdd228d
Nikita Pestrak <pestraknv@yandex.ru>
2021-03-13 00:28:02 +0300

    Task2Version2

---
+++ Sem2/C#/LR2/Task2Version2/Task2Version2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task2Version2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string linst = "abcdefghijklmnopqrstuvwxyz";
+            string total;
+            int sign, length;
+            Random x = new Random();
+            length = x.Next(1, 4); // If 4 inclusive, then change the string to (length = x.Next(1, 5);)
+            
+            for (int i = 0; i < length; i++)
+            {
+                sign = x.Next(0, 26);
+                total = string.Concat(linst[sign]);
+                Console.Write(total);
+            }
+        }
+    }
+}
