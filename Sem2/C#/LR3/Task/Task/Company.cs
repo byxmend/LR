850c0d134e725c2ff0f07dff8c16e11b6bee9974
Nikita Pestrak <pestraknv@yandex.ru>
2021-03-15 22:11:28 +0300

    LR3

--- Sem2/C#/LR3/Task/Task/Company.cs
+++ Sem2/C#/LR3/Task/Task/Company.cs
@@ -2,31 +2,31 @@
 
 namespace Task
 {
-	class Company
-	{
-		public string Name { get; set; }
-		public int AvarageSalary { get; set; }
-		public string WebsiteDomain { get; set; }
-		protected string ID { get; set; }
+    class Company
+    {
+        public string Name { get; set; }
+        public int AvarageSalary { get; set; }
+        public string WebsiteDomain { get; set; }
+        private string ID { get; set; }
 
-		public Company()
-		{
-			Name = "Priorbank";
-			AvarageSalary = 1000;
-			WebsiteDomain = "by";
-			ID = GenerationID();
-		}
+        public Company()
+        {
+            Name = "Priorbank";
+            AvarageSalary = 1000;
+            WebsiteDomain = "by";
+            ID = GenerationID();
+        }
 
-		public Company(string name = "", int avaragesalary = 0, string websitedomain = "")
-		{
-			Name = name;
-			AvarageSalary = avaragesalary;
-			WebsiteDomain = websitedomain;
-			ID = GenerationID();
-		}
+        public Company(string name = "", int avaragesalary = 0, string websitedomain = "")
+        {
+            Name = name;
+            AvarageSalary = avaragesalary;
+            WebsiteDomain = websitedomain;
+            ID = GenerationID();
+        }
 
-		protected static string GenerationID() => Guid.NewGuid().ToString();
+        private static string GenerationID() => Guid.NewGuid().ToString();
 
-		public override string ToString() => $"Name: {Name}\nAvarage salary: {AvarageSalary}\nWebsite domain: {WebsiteDomain}\nID: {ID}";
-	}
+        public override string ToString() => $"Name: {Name}\nAvarage salary: {AvarageSalary}\nWebsite domain: {WebsiteDomain}\nID: {ID}";
+    }
 }
--- Sem2/C#/LR3/Task/Task/Program.cs
+++ Sem2/C#/LR3/Task/Task/Program.cs
@@ -2,47 +2,47 @@
 
 namespace Task
 {
-	class Program
-	{
-		static void FillArray(Company[] arr)
-		{
-			for (int i = 0; i < arr.Length; i++)
-			{
-				arr[i] = new Company();
-				Console.Write("Name: ");
-				arr[i].Name = Console.ReadLine();
-				Console.Write("AvarageSalary: ");
-				arr[i].AvarageSalary = CheckInteger();
-				Console.Write("WebsiteDomain: ");
-				arr[i].WebsiteDomain = Console.ReadLine();
-			}
-		}
+    class Program
+    {
+        static void FillArray(Company[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new Company();
+                Console.Write("Name: ");
+                arr[i].Name = Console.ReadLine();
+                Console.Write("AvarageSalary: ");
+                arr[i].AvarageSalary = CheckInteger();
+                Console.Write("WebsiteDomain: ");
+                arr[i].WebsiteDomain = Console.ReadLine();
+            }
+        }
 
-		static int CheckInteger()
-		{
-			int num;
-			while (!int.TryParse(Console.ReadLine(), out num) || num <= 0)
-			{
-				Console.WriteLine("Error, enter the data again");
-			}
-			return num;
-		}
+        static int CheckInteger()
+        {
+            int num;
+            while (!int.TryParse(Console.ReadLine(), out num) || num <= 0)
+            {
+                Console.WriteLine("Error, enter the data again");
+            }
+            return num;
+        }
 
-		static void Main()
-		{
-			Company obj1 = new Company();
-			Company obj2 = new Company("Wargaming", 2000, "com");
-			Console.WriteLine(obj1 + "\n");
-			Console.WriteLine(obj2 + "\n");
+        static void Main()
+        {
+            Company obj1 = new Company();
+            Company obj2 = new Company("Wargaming", 2000, "com");
+            Console.WriteLine(obj1 + "\n");
+            Console.WriteLine(obj2 + "\n");
 
-			Console.WriteLine("Do you want to change the values? (yes - more then 1)");
-			int decision;
-			if (int.TryParse(Console.ReadLine(), out decision) && decision >= 1)
-			{
-				Company[] arr = new Company[2];
-				FillArray(arr);
-				Console.WriteLine();
-			}
-		}
-	}
+            Console.WriteLine("Do you want to change the values? (yes - more then 1)");
+            int decision;
+            if (int.TryParse(Console.ReadLine(), out decision) && decision >= 1)
+            {
+                Company[] arr = new Company[2];
+                FillArray(arr);
+                Console.WriteLine();
+            }
+        }
+    }
 }
