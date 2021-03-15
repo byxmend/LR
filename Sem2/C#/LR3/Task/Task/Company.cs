8f2032b1b7f7a5c70e63a892c131f8d2e372c65d
Nikita Pestrak <pestraknv@yandex.ru>
2021-03-15 21:22:30 +0300

    c#LR3

---
+++ Sem2/C#/LR3/Task/Task/Company.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task
+{
+    class Company
+    {
+        public string Name { get; set; }
+        public int AvarageSalary { get; set; }
+        public string WebsiteDomain { get; set; }
+        protected string ID { get; set; }
+
+        public Company()
+        {
+            Name = "Priorbank";
+            AvarageSalary = 1000;
+            WebsiteDomain = "by";
+            ID = GenerationID();
+        }
+
+        public Company(string name = "", int avaragesalary = 0, string websitedomain = "")
+        {
+            Name = name;
+            AvarageSalary = avaragesalary;
+            WebsiteDomain = websitedomain;
+            ID = GenerationID();
+        }
+
+        protected static string GenerationID() => Guid.NewGuid().ToString();
+
+        public override string ToString() => $"Name: {Name}\nAvarage salary: {AvarageSalary}\nWebsite domain: {WebsiteDomain}\nID: {ID}";
+    }
+}
---
+++ Sem2/C#/LR3/Task/Task/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Task
+{
+    class Program
+    {
+        static void FillArray(Company[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new Company();
+                Console.Write("Name: ");
+                arr[i].Name = Console.ReadLine();
+                Console.Write("AvarageSalary: ");
+                arr[i].AvarageSalary = CheckInteger();
+                Console.Write("WebsiteDomain: ");
+                arr[i].WebsiteDomain = Console.ReadLine();
+            }
+        }
+
+        static int CheckInteger()
+        {
+            int num;
+            while (!int.TryParse(Console.ReadLine(), out num) || num <= 0)
+            {
+                Console.WriteLine("Error, enter the data again");
+            }
+            return num;
+        }
+
+        static void Main()
+        {
+            Company obj1 = new Company();
+            Company obj2 = new Company("Wargaming", 2000, "com");
+            Console.WriteLine(obj1 + "\n");
+            Console.WriteLine(obj2 + "\n");
+
+            Console.WriteLine("Do you want to change the values? (1 - yes/ other - no)");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a == 1)
+            {
+                Company[] arr = new Company[2];
+                FillArray(arr);
+                Console.WriteLine();
+            }
+        }
+    }
+}
