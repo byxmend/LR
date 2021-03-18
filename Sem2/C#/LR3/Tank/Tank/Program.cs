f0c990912b4dbdfa0b7875c06e4f3700e0415d15
Nikita Pestrak <pestraknv@yandex.ru>
2021-03-18 13:48:14 +0300

    :alien:

--- Sem2/C#/LR3/Tank/Tank/Program.cs
+++ Sem2/C#/LR3/Tank/Tank/Program.cs
@@ -4,12 +4,41 @@ namespace Tank
 {
     class Program
     {
-        private static void Meny()
+        static void TanksFill(ref Tank[] array)
         {
-            Console.WriteLine("\n1 - add equipment\n2 - remove equipment\n3 - output all fields\n4 - turn off");
+            int AverageSpeed;
+            int Caliber;
+            int HitPoints;
+            int DamagePerMinute;
+            string Name;
+            string Armor;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Enter the average speed:");
+                AverageSpeed = CheckInt();
+                Console.WriteLine("Enter the caliber:");
+                Caliber = CheckInt();
+                Console.WriteLine("Enter the hit points:");
+                HitPoints = CheckInt();
+                Console.WriteLine("Enter the damage per minute:");
+                DamagePerMinute = CheckInt();
+                Console.WriteLine("Enter the name:");
+                Name = Console.ReadLine();
+                Console.WriteLine("Enter the armor:");
+                Armor = Console.ReadLine();
+
+                array[i] = new Tank(AverageSpeed, Caliber, HitPoints, DamagePerMinute, Name, Armor);
+                Console.Clear();
+            }
         }
         
-        static int CheckFloat()
+        private static void Meny()
+        {
+            Console.WriteLine("\n1 - add equipment\n2 - remove equipment\n3 - output all tanks\n4 - turn off");
+        }
+        
+        static int CheckInt()
         {
             int a;
             while (!int.TryParse(Console.ReadLine(), out a))
@@ -18,29 +47,56 @@ namespace Tank
             }
             return a;
         }
-        
-        static void Main(string[] args)
+
+        static void ShowTanksSet(Tank[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"\nAverageSpeed: {array[i].AverageSpeed}\nCaliber: {array[i].Caliber}\n" +
+                                  $"HitPoints: {array[i].HitPoints}\nDamagePerMinute: {array[i].DamagePerMinute}\n" +
+                                  $"Name: {array[i].Name}\nArmor: {array[i].Armor}\nId: {array[i].Id}");
+            }
+        }
+
+        static int ChooseTanks(Tank[] array)
         {
             int a;
-            
-            Tank x = new Tank();
-            x.Output();
+            Console.WriteLine("Choose number of the tank: ");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write((i + 1) + " ");
+            }
+
+            a = CheckInt();
+            return (a - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            int a, n;
+            Console.WriteLine("How many tanks?");
+            n = CheckInt();
+
+            Tank[] tanksSet = new Tank[n];
+            TanksFill(ref tanksSet);
+            ShowTanksSet(tanksSet);
             
             while (true)
             {
+                Console.Clear();
                 Meny();
-                a = CheckFloat();
-                
+                a = CheckInt();
+
                 switch (a)
                 {
                     case 1:
-                        x.AddEquipment();
+                        tanksSet[ChooseTanks(tanksSet)].AddEquipment();
                         break;
                     case 2:
-                        x.RemoveEquipment();
+                        tanksSet[ChooseTanks(tanksSet)].RemoveEquipment();
                         break;
                     case 3:
-                        x.Output();
+                        ShowTanksSet(tanksSet);
                         break;
                     case 4:
                         return;
--- Sem2/C#/LR3/Tank/Tank/Tank.cs
+++ Sem2/C#/LR3/Tank/Tank/Tank.cs
@@ -4,57 +4,37 @@ namespace Tank
 {
     public class Tank
     {
-        private int AverageSpeed { get; set; }
-        private int Caliber { get; }
-        private int HitPoints { get; set; }
-        private int DamagePerMinute { get; set; }
-        private string Name { get; }
-        private string Armor { get; }
-        private string Id { get; }
+        public int AverageSpeed { get; set; }
+        public int Caliber { get; }
+        public int HitPoints { get; set; }
+        public int DamagePerMinute { get; set; }
+        public string Name { get; }
+        public string Armor { get; }
+        public string Id { get; }
 
-        public Tank()
+        public Tank(int averageSpeed, int caliber, int hitPoints, int damagePerMinute, string name, string armor)
         {
-            AverageSpeed = 30;
-            Caliber = 150;
-            HitPoints = 2750;
-            DamagePerMinute = 2465;
-            Name = "E100";
-            Armor = "good";
+            AverageSpeed = averageSpeed;
+            Caliber = caliber;
+            HitPoints = hitPoints;
+            DamagePerMinute = damagePerMinute;
+            Name = name;
+            Armor = armor;
             Id = GenerationId();
         }
 
         public void AddEquipment()
         {
-            if (AverageSpeed == 35)
-            {
-                Console.WriteLine("There is equipment on the tank");
-            }
-            else
-            {
-                AverageSpeed += 5;
-                HitPoints += 165;
-                DamagePerMinute += 186;
-            }
+            AverageSpeed += 5;
+            HitPoints += 165;
+            DamagePerMinute += 186;
         }
 
         public void RemoveEquipment()
         {
-            if (AverageSpeed == 30)
-            {
-                Console.WriteLine("There is no equipment on the tank");
-            }
-            else
-            {
-                AverageSpeed -= 5;
-                HitPoints -= 165;
-                DamagePerMinute -= 186;
-            }
-        }
-
-        public void Output()
-        {
-            Console.WriteLine($"\nAverageSpeed: {AverageSpeed}\nCaliber: {Caliber}\nHitPoints: {HitPoints}\n" +
-                              $"DamagePerMinute: {DamagePerMinute}\nName: {Name}\nArmor: {Armor}\nId: {Id}");
+            AverageSpeed -= 5;
+            HitPoints -= 165;
+            DamagePerMinute -= 186;
         }
 
         private static string GenerationId() => System.Guid.NewGuid().ToString();
