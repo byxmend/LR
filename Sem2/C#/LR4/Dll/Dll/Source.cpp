32e006f4da11e67e1c9c49f74df0318ac18902b5
Nikita Pestrak <pestraknv@yandex.ru>
2021-04-01 03:39:57 +0300

    :+1:

--- Sem2/C#/LR4/Dll/Dll/Source.cpp
+++ Sem2/C#/LR4/Dll/Dll/Source.cpp
@@ -6,25 +6,25 @@
 
 int Add(int a, int b)
 {
-	return (a + b);
+    return (a + b);
 }
 
 int Subtract(int a, int b)
 {
-	return (a - b);
+    return (a - b);
 }
 
 int Multiply(int a, int b)
 {
-	return (a * b);
+    return (a * b);
 }
 
 int Division(int a, int b)
 {
-	return (a / b);
+    return (a / b);
 }
 
 int RemainderOfTheDivision(int a, int b)
 {
-	return (a % b);
+    return (a % b);
 }
