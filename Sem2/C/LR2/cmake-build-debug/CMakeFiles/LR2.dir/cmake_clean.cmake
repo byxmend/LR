7f06d4bda60fbbb0ff070db12432d2072586d9f9
Nikita Pestrak <pestraknv@yandex.ru>
2021-04-03 16:30:56 +0300

    LR2 Task1 option 12

---
+++ Sem2/C/LR2/cmake-build-debug/CMakeFiles/LR2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LR2.dir/main.c.obj"
+  "LR2.exe"
+  "LR2.exe.manifest"
+  "LR2.lib"
+  "LR2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LR2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
