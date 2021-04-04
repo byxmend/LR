b2a6a1d3c9e5339e3308fd75b4bb2f42de5e4715
Nikita Pestrak <pestraknv@yandex.ru>
2021-04-04 13:33:26 +0300

    :apple:

---
+++ Sem2/C/LR3/cmake-build-debug/CMakeFiles/LR3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LR3.dir/main.c.obj"
+  "LR3.exe"
+  "LR3.exe.manifest"
+  "LR3.lib"
+  "LR3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LR3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Sem2/C/LR3/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+#include <stdlib.h>
+
+int main( )
+{
+    int stringFirstMatrix, stringSecondMatrix = 0;
+    int columnFirstMatrix, columnSecondMatrix = 0;
+    int i, j, k, w, a, b, x, y;
+    int *firstMatrix;
+
+    printf("Size matrix:\n");
+    printf("String:");
+    scanf_s("%d", &stringFirstMatrix);
+    printf("Column:");
+    scanf_s("%d", &columnFirstMatrix);
+
+
+
+    return 0;
+}
+
+
+// SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE);
+// srand( time(0) );
+//    int r = rand() % 2;
+//    printf("%d", r);
