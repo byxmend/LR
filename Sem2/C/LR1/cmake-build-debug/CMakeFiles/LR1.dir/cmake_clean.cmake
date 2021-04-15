997b0f698b2cefd4ee11a7b9d6ba7a3779182be0
Nikita Pestrak <pestraknv@yandex.ru>
2021-04-15 13:56:17 +0300

    :airplane:

---
+++ Sem2/C/LR1/cmake-build-debug/CMakeFiles/LR1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LR1.dir/main.c.obj"
+  "LR1.exe"
+  "LR1.exe.manifest"
+  "LR1.lib"
+  "LR1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LR1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Sem2/C/LR3/main.c
+++ Sem2/C/LR3/main.c
@@ -5,10 +5,13 @@
 
 int main( )
 {
-    int stringFirstMatrix, stringSecondMatrix = 0;
-    int columnFirstMatrix, columnSecondMatrix = 0;
+    int stringFirstMatrix, stringSecondMatrix;
+    int columnFirstMatrix, columnSecondMatrix;
     int i, j, k, w, a, b, x, y;
-    int *firstMatrix;
+
+    srand(time(0));
+
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
 
     printf("Size matrix:\n");
     printf("String:");
@@ -16,13 +19,78 @@ int main( )
     printf("Column:");
     scanf_s("%d", &columnFirstMatrix);
 
+    int** array = (int**)malloc(sizeof(int*) * stringFirstMatrix);
+    for(i = 0; i < stringFirstMatrix; i++)
+        array[i] = (int*)malloc(sizeof(int*) * columnFirstMatrix);
 
+    printf("Random matrix:\n");
+
+    for (i = 0; i < stringFirstMatrix; i++) {
+        for (j = 0; j < columnFirstMatrix; j++) {
+            array[i][j] = rand() % 2;
+            printf("%d ", array[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Size matrix (all 0):\n");
+    printf("String:");
+    scanf_s("%d", &stringSecondMatrix);
+    printf("Column:");
+    scanf_s("%d", &columnSecondMatrix);
+
+    printf("Find matrix:\n");
+
+    x = columnFirstMatrix - columnSecondMatrix;
+    y = stringFirstMatrix - stringSecondMatrix;
+    w = 0;
+
+    for (b = 0; b < y + 1; b++)
+    {
+        for (a = 0; a < x + 1; a++)
+        {
+            k = 0;
+
+            for (i = b; i < stringSecondMatrix + b; i++)
+            {
+                for (j = a; j < columnSecondMatrix + a; j++)
+                {
+                    if (array[i][j] == 0)
+                        k++;
+                }
+            }
+
+            if (k == stringSecondMatrix * columnSecondMatrix)
+            {
+                w++;
+
+                printf("Case:%d\n", w);
+
+                for (x = 0; x < stringFirstMatrix; x++)
+                {
+                    for (y = 0; y < columnFirstMatrix; y++)
+                    {
+                        if (((x >= b) && (x < i)) && ((y >= a) && (y < j)))
+                        {
+                            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE);
+                            printf("%d ", array[x][y]);
+                        }
+                        else
+                        {
+                            SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_GREEN);
+                            printf("%d ", array[x][y]);
+                        }
+                    }
+
+                    printf("\n");
+                }
+            }
+        }
+    }
+
+    for(int i = 0; i < stringFirstMatrix; i++)
+        free(array[i]);
+    free(array);
 
     return 0;
 }
-
-
-// SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_BLUE);
-// srand( time(0) );
-//    int r = rand() % 2;
-//    printf("%d", r);
